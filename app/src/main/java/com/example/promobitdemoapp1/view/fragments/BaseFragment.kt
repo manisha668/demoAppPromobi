3486c5d0b15b3f29eab7355b73d87339b99d5a76
Manisha <manisha.sharma@gmail.com>
2021-02-14 23:28:23 +0530

    commiting project in git

---
+++ app/src/main/java/com/example/promobitdemoapp1/view/fragments/BaseFragment.kt
@@ -0,0 +1,24 @@
+package com.example.promobitdemoapp1.view.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.promobitdemoapp1.R
+
+class BaseFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_base, container, false)
+    }
+}
